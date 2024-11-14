e9c0d8eb1f79ac6f8ab3c02e852ef2643ad9065b
kauan-cancelier <kauanmellocancelier2005@gmail.com>
2024-11-14 19:37:49 -0300

    Criada estrutura base de relatorio.

---
+++ src/main/java/com/br/dreamday/controller/RelatorioController.java
@@ -0,0 +1,34 @@
+package com.br.dreamday.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RelatorioController {
+
+    @FXML
+    private Button btnCliente;
+
+    @FXML
+    private Button btnStatus;
+
+    @FXML
+    private Button btnValor;
+
+    @FXML
+    void aoClicarCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aoClicarStatus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aoClicarValor(ActionEvent event) {
+
+    }
+
+}
+
